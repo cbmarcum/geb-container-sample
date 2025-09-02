60d21ace0f64754430ef487aad40e8cf96990a3c
Carl Marcum <carl.marcum@codebuilders.net>
2025-09-02 17:44:42 -0400

    chore: add a regular geb test

---
+++ app/src/test/groovy/net/codebuilders/GebTestSpec.groovy
@@ -0,0 +1,33 @@
+package net.codebuilders
+
+import geb.spock.GebSpec
+import spock.lang.Shared
+
+/**
+ * See https://docs.grails.org/latest/guide/testing.html#functionalTesting and https://www.gebish.org/manual/current/
+ * for more instructions on how to write functional tests with Grails and Geb.
+ */
+
+class GebTestSpec extends GebSpec {
+
+    @Shared
+    serverPort = 8080
+
+    void 'should display the correct title on the home page'() {
+        when: 'visiting the home page'
+            go('https://groovy.apache.org/geb/')
+
+        then: 'the page title is correct'
+            title == 'Geb - Very Groovy Browser Automation'
+    }
+
+
+    void 'should display the correct title on the docs page'() {
+        when: 'visiting the home page'
+        go('https://groovy.apache.org/geb/manual/snapshot/')
+
+        then: 'the title is correct'
+        title == 'The Book Of Geb'
+    }
+
+}

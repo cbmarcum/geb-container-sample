83c1bc0968206a02a924fe0a2761431d8ce96004
Carl Marcum <carl.marcum@codebuilders.net>
2025-10-14 18:43:31 -0400

    chore: add test from geb debug work.

---
+++ app/src/test/groovy/net/codebuilders/CachingDriverFactorySpec.groovy
@@ -0,0 +1,54 @@
+package net.codebuilders
+
+
+import geb.driver.CachingDriverFactory
+import geb.driver.DriverFactory
+import geb.spock.GebSpec
+import org.openqa.selenium.NoSuchSessionException
+import org.openqa.selenium.WebDriver
+import spock.util.concurrent.BlockingVariable
+
+
+class CachingDriverFactorySpec extends GebSpec {
+
+    def "global driver and per-thread driver factories are independent"() {
+        given:
+        def stubFactory = Stub(DriverFactory)
+        def globalFactory = CachingDriverFactory.global(stubFactory, false)
+        def perThreadFactory = CachingDriverFactory.perThread(stubFactory, false)
+        def globalDriver2 = new BlockingVariable<WebDriver>()
+        def perThreadDriver2 = new BlockingVariable<WebDriver>()
+
+        when:
+        def globalDriver1 = globalFactory.driver
+        Thread.start { globalDriver2.set(globalFactory.driver) }
+        def perThreadDriver1 = perThreadFactory.driver
+        Thread.start { perThreadDriver2.set(perThreadFactory.driver) }
+
+        then:
+        globalDriver1 == globalDriver2.get()
+        globalDriver1 != perThreadDriver1
+        perThreadDriver1 != perThreadDriver2.get()
+    }
+
+
+    def "first driver should quit after clear cache and quit driver"() {
+
+        given:
+        def firstDriver= browser.driver
+
+        when:
+        CachingDriverFactory.clearCacheAndQuitDriver()
+        browser.driver = firstDriver
+        go('https://groovy.apache.org/geb/manual/snapshot/')
+
+        then:
+        thrown(NoSuchSessionException)
+
+    }
+
+    def cleanup() {
+        CachingDriverFactory.clearCacheAndQuitDriver()
+    }
+
+}
